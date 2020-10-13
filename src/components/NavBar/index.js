f566b35c6055f222d2f1f38d48322d1b6d2072e3
Juan Camilo Rico <juancarico92@gmail.com>
2020-10-13 17:58:05 -0500

    [FEAT] NavBar componentn created and styles applied

---
+++ src/components/NavBar/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Nav } from './styles'
+
+export const NavBar = () => {
+  return (
+    <Nav>
+      <button>Home</button>
+      <button>Favs</button>
+      <button>User</button>
+    </Nav>
+  )
+}
