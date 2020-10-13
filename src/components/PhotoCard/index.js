c9274efd013ccd0f0d93536839c0329279dda723
Juan Camilo Rico <juancarico92@gmail.com>
2020-10-13 12:09:08 -0500

    [FEAT] FavButton component created for refactoring like button in PhotoCard

--- src/components/PhotoCard/index.js
+++ src/components/PhotoCard/index.js
@@ -1,8 +1,10 @@
 import React from 'react'
-import { Article, ImgWrapper, Img, Button } from './styles'
-import { MdFavoriteBorder, MdFavorite } from 'react-icons/md'
+import { Article, ImgWrapper, Img } from './styles'
+
 import { useLocalStorage } from '../../hooks/useLocalStorage'
 import { useNearScreen } from '../../hooks/useNearScreen'
+
+import { FavButton } from '../FavButton'
 
 const DEFAULT_IMAGE =
   'https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60'
@@ -12,7 +14,7 @@ export const PhotoCard = ({ id, likes = 0, src = DEFAULT_IMAGE }) => {
   const key = `like-${id}`
   const [liked, setLiked] = useLocalStorage(key, false)
 
-  const Icon = liked ? MdFavorite : MdFavoriteBorder
+  const handleFavClick = () => setLiked(!liked)
 
   return (
     <Article ref={element}>
@@ -25,10 +27,7 @@ export const PhotoCard = ({ id, likes = 0, src = DEFAULT_IMAGE }) => {
           </a>
         </>
       )}
-
-      <Button onClick={() => setLiked(!liked)}>
-        <Icon size='32px' /> {likes} likes!
-      </Button>
+      <FavButton liked={liked} likes={likes} onClick={handleFavClick} />
     </Article>
   )
 }
