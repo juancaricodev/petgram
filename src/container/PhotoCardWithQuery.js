36ca059ae23c0fc59fb3e0e97c1bb6ea1d461331
Juan Camilo Rico <juancarico92@gmail.com>
2020-10-13 11:47:24 -0500

    [REFACTOR] PhotoCardWithQuery: query and render prop refactored.  withPhotos: query refactored

--- src/container/PhotoCardWithQuery.js
+++ src/container/PhotoCardWithQuery.js
@@ -4,9 +4,9 @@ import { PhotoCard } from '../components/PhotoCard'
 import { gql } from 'apollo-boost'
 import { Query } from 'react-apollo'
 
-const query = gql`
-  query getSinglePhoto($id:ID!) {
-    photo(id:$id) {
+const GET_SINGLE_PHOTO = gql`
+  query getSinglePhoto($id: ID!) {
+    photo(id: $id) {
       id
       categoryId
       src
@@ -17,13 +17,16 @@ const query = gql`
   }
 `
 
-export const PhotoCardWithQuery = (id) => (
-  <Query query={query} variables={id}>
-    {
-      ({ loading, error, data }) => {
-        const { photo = {} } = !!data && data
-        return <PhotoCard {...photo} />
-      }
-    }
+const renderProp = ({ loading, error, data }) => {
+  if (loading) return <p>Loading...</p>
+  if (error) return <p>Error!</p>
+
+  const { photo = {} } = !!data && data
+  return <PhotoCard {...photo} />
+}
+
+export const PhotoCardWithQuery = ({ id }) => (
+  <Query query={GET_SINGLE_PHOTO} variables={{ id }}>
+    {renderProp}
   </Query>
 )
