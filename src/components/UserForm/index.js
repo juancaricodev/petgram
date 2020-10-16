ba3b278ff5c59b4c6f952d13d6f90d715d2fe5c5
Juan Camilo Rico <juancarico92@gmail.com>
2020-10-16 11:25:03 -0500

    [FEAT] styles added to UseForm, and form split in registering and logging in

--- src/components/UserForm/index.js
+++ src/components/UserForm/index.js
@@ -1,23 +1,27 @@
 import React from 'react'
 import { useInputValue } from '../../hooks/useInputValue'
+import { Form, Input, Button, Title } from './styles'
 
-export const UserForm = ({ onSubmit }) => {
+export const UserForm = ({ onSubmit, title }) => {
   const email = useInputValue('')
   const password = useInputValue('')
 
   return (
-    <form onSubmit={onSubmit}>
-      <input
-        placeholder='Email'
-        type='email'
-        {...email}
-      />
-      <input
-        placeholder='Password'
-        type='password'
-        {...password}
-      />
-      <button>Login</button>
-    </form>
+    <>
+      <Title>{title}</Title>
+      <Form onSubmit={onSubmit}>
+        <Input
+          placeholder='Email'
+          type='email'
+          {...email}
+        />
+        <Input
+          placeholder='Password'
+          type='password'
+          {...password}
+        />
+        <Button>{title}</Button>
+      </Form>
+    </>
   )
 }
